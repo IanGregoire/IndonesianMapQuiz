acbda2dae7fd2c9d7b92e8977ba45ebda4078efe
IanGregoire <ilghbr@gmail.com>
2025-06-04 11:43:47 -0400

    Feat: add topo json files for each separate island

--- src/components/indonesiaMap.tsx
+++ src/components/indonesiaMap.tsx
@@ -1,16 +1,23 @@
 "use client";
 import { ComposableMap, Geographies, Geography } from "react-simple-maps";
 
-const geoUrl = "/maps/gadm41_IDN_2_Topo.json";
+const geoUrl = "/maps/gadm41_IDN_2_Topo.json"; // center: [118, -5], scale: 950,
+const Borneo = "/maps/Borneo.json";  // center: [112, -2], scale: 2550,
+const Maluku = "/maps/Maluku.json"; //center: [130, -7], scale: 3550,
+const Java = "/maps/Java.json"; // center: [111, -9], scale: 3650,
+const Nusa = "/maps/Nusa.json"; // center: [120, -10], scale: 3850,
+const Sulawesi = "/maps/Sulawesi.json"; // center: [120, -5], scale: 2550,
+const Sumatra = "/maps/Sumatra.json"; // center: [100, -3], scale: 1850,
+const WestPapua = "/maps/West_Papua.json"; // center: [134, -5], scale: 2150,
 
 export default function IndonesiaMap() {
   return (
     <ComposableMap projection="geoMercator" 
     projectionConfig={{
-    center: [118, -5],
-    scale: 950,
-  }}>
-      <Geographies geography={geoUrl}>
+    center: [134, -5],
+    scale: 2150,
+    }}>
+      <Geographies geography={WestPapua}>
         {({ geographies }) =>
           geographies.map((geo) => (
             <Geography
